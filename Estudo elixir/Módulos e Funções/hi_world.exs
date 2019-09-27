775181fec0b9f8a703e784ee578b092d840c984f
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-09-26 22:00:43 -0400

    Update method with directive imported

--- Estudo elixir/Módulos e Funções/hi_world.exs
+++ Estudo elixir/Módulos e Funções/hi_world.exs
@@ -3,6 +3,6 @@ defmodule HiWorld do
     import IO, only: [puts: 1]
 
     def say do
-        IO.puts "Hey, its my first code with elixir"
+        puts "Hey, its my first code with elixir"
     end
 end
