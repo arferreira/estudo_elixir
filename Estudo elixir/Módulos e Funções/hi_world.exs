9ac5c9821ca34fb84a6429e5696391936b1e6156
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-09-26 22:06:47 -0400

    Call function inside other function

--- Estudo elixir/Módulos e Funções/hi_world.exs
+++ Estudo elixir/Módulos e Funções/hi_world.exs
@@ -1,8 +1,15 @@
 defmodule HiWorld do
     
     import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
 
-    def say do
-        puts "Hey, its my first code with elixir"
+    def say_hi do
+        inspect("Chamando inspect de dentro de say_hi")
+    end
+
+    def inspect(param) do
+        puts "Entrando na função"
+        puts param
+        puts "Saindo da função"
     end
 end
